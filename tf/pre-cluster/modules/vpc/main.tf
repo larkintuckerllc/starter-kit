587391eeec052ec48eeef0224fddf1486d23e67b
John Tucker <john@larkintuckerllc.com>
2020-09-18 12:22:02 -0400

    route tables

--- tf/pre-cluster/modules/vpc/main.tf
+++ tf/pre-cluster/modules/vpc/main.tf
@@ -27,8 +27,7 @@ locals {
 resource "aws_vpc" "this" {
   cidr_block = local.vpc_cidr_block
   tags = {
-    Name    = var.identifier
-    Project = var.identifier
+    Infrastructure = var.identifier
   }
 }
 
@@ -41,8 +40,8 @@ resource "aws_subnet" "private" {
   tags = {
     "kubernetes.io/cluster/${var.identifier}" = "shared"
     "kubernetes.io/role/internal-elb"         = "1"
-    Name                                      = "${var.identifier}-private-${each.key}"
     Infrastructure                            = var.identifier
+    Name                                      = "${var.identifier}-private-${each.key}"
     Tier                                      = "private"
   }
   vpc_id                  = aws_vpc.this.id
@@ -56,121 +55,84 @@ resource "aws_subnet" "public" {
   tags = {
     "kubernetes.io/cluster/${var.identifier}" = "shared"
     "kubernetes.io/role/elb"                  = "0"
-    Name                                      = "${var.identifier}-public-${each.key}"
     Infrastructure                            = var.identifier
+    Name                                      = "${var.identifier}-public-${each.key}"
     Tier                                      = "public"
   }
   vpc_id                  = aws_vpc.this.id
 }
 
-/*
 # GATEWAYS
 
 resource "aws_internet_gateway" "this" {
   tags = {
-    Name    = var.identifier
-    Project = var.identifier
+    Infrastructure = var.identifier
+    Name           = var.identifier
   }
   vpc_id = aws_vpc.this.id
 }
 
-resource "aws_eip" "pub_0" {
+resource "aws_eip" "this" {
+  for_each   = local.public_subnet
   depends_on = [aws_internet_gateway.this]
   tags = {
-    Name    = "${var.identifier}-pub_0"
-    Project = var.identifier
+    Infrastructure = var.identifier
+    Name           = "${var.identifier}-${each.key}"
   }
   vpc        = true
 }
 
-resource "aws_eip" "pub_1" {
-  count      = var.ha ? 1 : 0
-  depends_on = [aws_internet_gateway.this]
+resource "aws_nat_gateway" "this" {
+  for_each      = local.public_subnet
+  allocation_id = aws_eip.this[each.key].id
+  subnet_id     = aws_subnet.public[each.key].id
   tags = {
-    Name    = "${var.identifier}-pub_1"
-    Project = var.identifier
-  }
-  vpc        = true
-}
-
-resource "aws_nat_gateway" "pub_0" {
-  allocation_id = aws_eip.pub_0.id
-  subnet_id     = aws_subnet.pub_0.id
-  tags = {
-    Name    = "${var.identifier}-pub_0"
-    Project = var.identifier
-  }
-}
-
-resource "aws_nat_gateway" "pub_1" {
-  count         = var.ha ? 1 : 0
-  allocation_id = aws_eip.pub_1[0].id
-  subnet_id     = aws_subnet.pub_1.id
-  tags = {
-    Name    = "${var.identifier}-pub_1"
-    Project = var.identifier
+    Infrastructure = var.identifier
+    Name           = "${var.identifier}-${each.key}"
   }
 }
 
 # ROUTE TABLES
 
-resource "aws_route_table" "pub" {
-  vpc_id = aws_vpc.this.id
+resource "aws_route_table" "private" {
+  for_each = local.private_subnet
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.this[each.key].id
+  }
+  tags = {
+    Infrastructure = var.identifier
+    Name           = "${var.identifier}-private-${each.key}"
+  }
+  vpc_id   = aws_vpc.this.id
+}
+
+resource "aws_route_table" "public" {
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.this.id
   }
   tags = {
-    Name    = "${var.identifier}-pub"
-    Project = var.identifier
+    Infrastructure = var.identifier
+    Name           = "${var.identifier}-public"
   }
-}
-
-resource "aws_route_table" "prv_0" {
   vpc_id = aws_vpc.this.id
-  route {
-    cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.pub_0.id
-  }
-  tags = {
-    Name    = "${var.identifier}-prv_0"
-    Project = var.identifier
-  }
 }
 
-resource "aws_route_table" "prv_1" {
-  vpc_id = aws_vpc.this.id
-  route {
-    cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = var.ha ? aws_nat_gateway.pub_1[0].id : aws_nat_gateway.pub_0.id
-  }
-  tags = {
-    Name    = "${var.identifier}-prv_1"
-    Project = var.identifier
-  }
+resource "aws_route_table_association" "private" {
+  for_each = local.private_subnet
+  route_table_id = aws_route_table.private[each.key].id
+  subnet_id      = aws_subnet.private[each.key].id
 }
 
-resource "aws_route_table_association" "pub_0" {
-  subnet_id      = aws_subnet.pub_0.id
-  route_table_id = aws_route_table.pub.id
+resource "aws_route_table_association" "public" {
+  for_each = local.public_subnet
+  route_table_id = aws_route_table.public.id
+  subnet_id      = aws_subnet.public[each.key].id
 }
 
-resource "aws_route_table_association" "pub_1" {
-  subnet_id      = aws_subnet.pub_1.id
-  route_table_id = aws_route_table.pub.id
-}
-
-resource "aws_route_table_association" "prv_0" {
-  subnet_id      = aws_subnet.prv_0.id
-  route_table_id = aws_route_table.prv_0.id
-}
-
-resource "aws_route_table_association" "prv_1" {
-  subnet_id      = aws_subnet.prv_1.id
-  route_table_id = aws_route_table.prv_1.id 
-}
-
-# NETWORK ACLS
+/*
+# NETWORK ACL
 
 resource "aws_network_acl" "this" {
   egress {
