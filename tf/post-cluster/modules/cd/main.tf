fac4431416f96fef7d224a0f7e0b016605d0a884
John Tucker <john@larkintuckerllc.com>
2020-09-23 07:36:22 -0400

    fix docker go

--- tf/post-cluster/modules/cd/main.tf
+++ tf/post-cluster/modules/cd/main.tf
@@ -16,11 +16,15 @@ locals {
   name                 = "workload"
   platform_dockerfile = {
     go     = <<EOF
-          FROM golang:1.14
+          FROM golang:1.14 AS builder
           WORKDIR /go/src/app
+          COPY go.mod go.sum ./
+          RUN go mod download
           COPY . .
-          RUN go get -d -v ./...
-          RUN go install -v ./...
+          RUN CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go install -v ./cmd/...
+
+          FROM alpine:3.12
+          COPY --from=builder /go/bin/* /usr/local/bin/
           EXPOSE 8080
           USER 1000:1000
           ENV PORT=8080
