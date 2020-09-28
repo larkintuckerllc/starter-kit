8117bde458caa83bda2ed1f7c56441e97dbd4d3e
John Tucker <john@larkintuckerllc.com>
2020-09-28 14:16:51 -0400

    output

--- tf/post-cluster/modules/aurora/outputs.tf
+++ tf/post-cluster/modules/aurora/outputs.tf
@@ -1,3 +1,15 @@
+output "mydb_database_name" {
+  value = aws_rds_cluster.mydb.database_name
+}
+
 output "mydb_endpoint" {
   value = aws_rds_cluster.mydb.endpoint
+}
+
+output "mydb_master_password" {
+  value = aws_rds_cluster.mydb.master_password
+}
+
+output "mydb_master_username" {
+  value = aws_rds_cluster.mydb.master_username
 }
