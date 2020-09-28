be0054d2de23bf47aeb59da74c04f4c8bdb5e3b5
John Tucker <john@larkintuckerllc.com>
2020-09-28 06:08:49 -0400

    sg

--- tf/post-cluster/modules/aurora/main.tf
+++ tf/post-cluster/modules/aurora/main.tf
@@ -12,13 +12,26 @@ data "aws_subnet_ids" "this" {
   vpc_id = data.aws_vpc.this.id
 }
 
-/*
-resource "aws_db_subnet_group" "default" {
-  name       = "main"
-  subnet_ids = [aws_subnet.frontend.id, aws_subnet.backend.id]
-
+resource "aws_db_subnet_group" "this" {
+  name       = var.identifier
+  subnet_ids = data.aws_subnet_ids.this.ids
   tags = {
-    Name = "My DB subnet group"
+    Infrastructure = var.identifier
   }
 }
-*/
+
+# TODO: FIX SECURITY GROUP TO CLUSTER
+resource "aws_security_group" "this" {
+  ingress {
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 5432
+    protocol    = "tcp"
+    to_port     = 5432
+  }
+  name   = "${var.identifier}-postgresql"
+  tags = {
+    Infrastructure = var.identifier
+    Name           = "${var.identifier}-postgresql"
+  }
+  vpc_id = data.aws_vpc.this.id
+}
